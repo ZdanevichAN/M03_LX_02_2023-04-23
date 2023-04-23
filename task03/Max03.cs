be3128f94be9ab1bbf55d83857d2ca5c4c9adc84
Alecsander Zdanevich <alecsdjkzda@gmail.com>
2023-04-24 00:14:45 +0300

    Модуль_03_Лекция_02_наполнение массива рандомными числами и метод вывод их на экран

---
+++ task03/Max03.cs
@@ -0,0 +1,26 @@
+﻿//метод для наполнения массива рандомными значениями
+void FillArray(int[] collection) // - принимает массив(колекцию)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+//метод выводящий на экран
+void PrintArrays(int[] coll)
+{
+    int count = coll.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(coll[position]);
+        position++;
+    }
+}
+//создай новый массив в котором будет 10 пустых элементов 
+int[] array = new int[10];
+FillArray(array);
+PrintArrays(array);
