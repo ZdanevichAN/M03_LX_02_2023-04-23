035b298e8deabb4f0e9a24a8781672a65fe3a91d
Alecsander Zdanevich <alecsdjkzda@gmail.com>
2023-04-23 23:43:18 +0300

    Модуль_03_Лекция_02_поиск элемента массива равного find

--- task01/FindMax.cs
+++ task01/FindMax.cs
@@ -1,17 +1,1 @@
-﻿// поиск максимального через функцию
-int num1 = 45, num2 = 50, num3 = 12;
-int num4 = 32, num5 = 50, num6 = 1234;
-int num7 = 424, num8 = 789, num9 = -90;
-int Max(int arg1, int arg2, int arg3)
-{
-    int result = arg1;
-    if (result<arg2) result = arg2;
-    if (result<arg3) result = arg3;
-    return result;
-}
-int max = Max(
-            Max(num1, num2, num3),
-            Max(num4, num5, num6),
-            Max(num7, num8, num9)
-            );
-Console.Write(max);
+﻿.
---
+++ task02/MasFindMax.cs
@@ -0,0 +1,19 @@
+﻿// поиск элемента массива равного find
+int[] array = {12,456,24,
+                234,678,23,
+                45,11,0
+                };
+
+int n = array.Length;
+int find = 11;
+int index = 0;
+while(index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break; //завершить работу при первом вхождении
+    }
+
+    index++;
+}
